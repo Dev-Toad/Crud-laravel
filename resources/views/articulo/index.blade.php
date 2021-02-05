6e0e3837d2b8b5ae1536ed2104cdddd83fe2d957
Lardin Armas <andresarmas97@hotmail.com>
2021-02-04 23:36:52 -0500

    first commit

---
+++ resources/views/articulo/index.blade.php
@@ -0,0 +1,61 @@
+@extends('adminlte::page')
+
+@section('title', 'Crud')
+
+@section('content_header')
+    <h1>Listado de articulos</h1>
+@stop
+
+@section('content')
+<a href="articulos/create" class="btn btn-primary mb-3"> Crear</a>
+   <table id="articulos" class="table table-striped table-bordered shadow-lg mt-4" style="width:100%">
+        <thead class="bg-primary text-white">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Codigo</th>
+                <th scope="col">Descripcion</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">precio</th>
+                <th scope="col">acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($articulos as $articulo)
+                <tr>
+                    <td>{{ $articulo->id}}</td>
+                    <td>{{ $articulo->codigo}}</td>
+                    <td>{{ $articulo->descripcion}}</td>
+                    <td>{{ $articulo->cantidad }}</td>
+                    <td>{{ $articulo->precio }}</td>
+                    <td>
+                    <form action="{{ route('articulos.destroy',$articulo->id) }}" method="POST">
+                        <a href="/articulos/{{ $articulo->id }}/edit" class="btn btn-info">Editar</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+   </table>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+    <link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css">
+@stop
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('#articulos').DataTable({
+                    "lengthMenu":[[5,10, 50, -1],[5, 10, 50, "Al"]]
+                });
+            } );
+        </script>
+@stop
